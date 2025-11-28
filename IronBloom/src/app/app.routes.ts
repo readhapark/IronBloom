b0d17df8f89efcc7ee9f2d37f57fb91699394fce
Shireen Ghazala <shireen.nu2022@gmail.com>
2025-11-28 15:36:39 -0600

    nav links fix

--- IronBloom/src/app/app.routes.ts
+++ IronBloom/src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { RecipeLibrary } from './recipe-library/recipe-library';
+import { Home } from './home/home';
 
 export const routes: Routes = [
     {
+    path: '',
+    component: Home
+    },
+    {
     path: 'recipes',
     component: RecipeLibrary
+    },
+    {
+    path: '**',
+    redirectTo: ''
   }
 ];
